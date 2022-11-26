ea12023a6552373a11982c0066bf411ee7d89414
Jayson De los Reyes <jisun.delosreyes@phitopolis.com>
2022-11-26 12:11:27 +0800

    setup storybook and add button component

---
+++ src/components/buttons/Button.tsx
@@ -0,0 +1,34 @@
+import { FC, PropsWithChildren } from "react";
+import { styled } from "../../stitches.config";
+
+const StyledButton = styled("button", {
+  position: "relative",
+  padding: "8px 16px",
+  fontWeight: "500",
+  fontsize: "12px",
+  lineHeight: "16px",
+  backgroundColor: "$white",
+  color: "$neutral700",
+  border: "1px solid $neutral400",
+  borderRadius: "3px",
+  "&:hover": {
+    color: "$neutral800",
+    borderColor: "$neutral600",
+  },
+  "&:active": {
+    color: "$neutral800",
+    backgroundColor: "$neutral100",
+    borderColor: "$neutral500",
+  },
+  "&:focus": {
+    color: "$neutral800",
+    borderColor: "$neutral600",
+    outline: "3px solid $blue200",
+  },
+});
+
+interface ButtonProps extends PropsWithChildren {}
+
+export const Button: FC<ButtonProps> = ({ children }) => {
+  return <StyledButton>{children}</StyledButton>;
+};
--- src/stitches.config.ts
+++ src/stitches.config.ts
@@ -1,8 +1,19 @@
 import { createStitches } from "@stitches/react";
 
-export const { styled, createTheme, getCssText, globalCss } = createStitches(
-  {}
-);
+export const { styled, createTheme, getCssText, globalCss } = createStitches({
+  theme: {
+    colors: {
+      white: "#ffffff",
+      neutral100: "#F4F6FA",
+      neutral400: "#D8DAE5",
+      neutral500: "#C1C4D6",
+      neutral600: "##8F95B2",
+      neutral700: "#696F8C",
+      neutral800: "#474D66",
+      blue200: "#D6E0FF",
+    },
+  },
+});
 
 export const globalStyles = globalCss({
   "*": {
